88dc423e17d8533792b93cc894c99109d6c3e26e
Elendow <elendow@gmail.com>
2016-06-28 19:16:01 +0200

    Tests passed

--- src/Turn.js
+++ src/Turn.js
@@ -2,12 +2,7 @@ const C = require('./constants.js')
 
 class Turn {
   constructor (board, bikes, inputs) {
-    this.board = board.slice()
     this.inputs = inputs
-    this.bikes = []
-    for (var i = 0; i < bikes.length; i++) {
-      this.bikes.push({ i: bikes[i].i, j: bikes[i].j, dir: bikes[i].dir })
-    }
   }
 
   setInput (p, dir) {
@@ -19,30 +14,13 @@ class Turn {
 
   evolve () {
     for (var i = 0; i < this.bikes.length; ++i) {
-      if (this.inputs[i] != null) {
-        this.bikes[i].dir = this.inputs[i]
       }
-      var posY = this.bikes[i].i
-      var posX = this.bikes[i].j
-      switch (this.bikes[i].dir) {
-        case C.UP: posY--
-          break
-        case C.DOWN: posY++
-          break
-        case C.LEFT: posX--
-          break
-        case C.RIGHT: posX++
-          break
       }
-      this.bikes[i].i = posY
-      this.bikes[i].j = posX
-      if (posX >= 0 && posY >= 0 && posX < this.board.length && posY < this.board.length) {
-        this.board[posY][posX] = i + 1
       } else {
-        console.log('Player ' + i + ' is dead')
       }
     }
     return new Turn(this.board, this.bikes, [null, null])
+    }
   }
 }
 exports.Turn = Turn
