55e6ec692352d11b03b7f7a2b79f90650fe26487
David da Silva Contín <dasilvacontin@Davids-MacBook-Pro-3.local>
2016-06-28 10:58:04 +0200

    Add more assertions

--- test/turn.spec.js
+++ test/turn.spec.js
@@ -12,12 +12,18 @@ test('Turn', (t) => {
   const inputs = [null]
   const turn = new Turn(board, bikes, inputs)
 
+  // turn.setInput(playerIndex, direction)
+  turn.setInput(0, C.DOWN)
   turn.setInput(0, C.UP)
   t.equal(turn.inputs[0], C.UP, 'setInput should update turns input')
 
   const nextTurn = turn.evolve()
   t.ok(nextTurn instanceof Turn, 'evolve should return an instance of Turn')
   t.notEqual(turn, nextTurn, 'evolve should return a new instance of Turn')
+  t.notEqual(turn.board, nextTurn.board, 'turns shouldnt share boards')
+  t.notEqual(turn.bikes, nextTurn.bikes, 'turns shouldnt share bikes')
+  t.notEqual(turn.inputs, nextTurn.inputs, 'turns shouldnt share inputs')
+
   t.deepEqual(nextTurn.board, [
     [1, 0, 0],
     [1, 0, 0],
