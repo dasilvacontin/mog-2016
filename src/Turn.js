dbaed4f99df4a44678b5e5970bfa422e4657ffd6
alicenara <alicenara165@gmail.com>
2016-06-28 12:11:58 +0200

    new tryin' hard  :last_quarter_moon_with_face:

--- src/Turn.js
+++ src/Turn.js
@@ -9,39 +9,39 @@ class Turn {
     this.inputs[num] = dir
   }
   evolve () {
-    for (var i = 0; i < this.inputs.length; ++i) {
-      if (this.inputs[i] !== null) {
-        this.bikes[num].dir = dir
-        switch (dir) {
+    for (var iter = 0; iter < this.inputs.length; ++iter) {
+      if (this.inputs[iter] !== null) {
+        this.bikes[iter].dir = this.inputs[iter]
+        switch (this.inputs[iter]) {
           case 1:
-            if (this.bikes[num].i > 0) {
-              this.bikes[num].i -= 1
+            if (this.bikes[iter].i > 0) {
+              this.bikes[iter].i -= 1
             }
             break
           case 2:
-            if (this.bikes[num].i < this.blenght) {
-              this.bikes[num].i += 1
+            if (this.bikes[iter].i < this.blenght) {
+              this.bikes[iter].i += 1
             }
             break
           case 3:
-            if (this.bikes[num].j < this.blenght) {
-              this.bikes[num].j += 1
+            if (this.bikes[iter].j < this.blenght) {
+              this.bikes[iter].j += 1
             }
             break
           case 4:
-            if (this.bikes[num].j > 0) {
-              this.bikes[num].j -= 1
+            if (this.bikes[iter].j > 0) {
+              this.bikes[iter].j -= 1
             }
             break
           default:
             throw new Error('Not a valid direction of movement')
         }
-        this.board[i][j] = 1
+        this.board[this.bikes[iter].i][this.bikes[iter].j] = iter + 1
       }
     }
-    newboard = this.board.map(lst => lst.slice())
-    newbikes = this.bikes.map(obj => Object.assign({},obj))
-    return new Turn(newboard,newbikes,[null])
-  }  
+    var newboard = this.board.map(lst => lst.slice())
+    var newbikes = this.bikes.map(obj => Object.assign({}, obj))
+    return new Turn(newboard, newbikes, [null, null])
+  }
 }
 exports.Turn = Turn
