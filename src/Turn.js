9301d6966b42e4ecbf11f806d93927bdc4b8abab
Guillem Ferrer PC-Uni <guillem.ferrer.nicolas@gmail.com>
2016-06-28 12:52:55 +0200

    thing

--- src/Turn.js
+++ src/Turn.js
@@ -1,4 +1,5 @@
 const C = require('../src/constants.js')
+var clone = require('clone')
 class Turn {
   constructor (board, bikes, inputs) {
     this.board = board
@@ -9,22 +10,27 @@ class Turn {
     this.inputs[pos] = input
   }
   evolve () {
-    var newBoard = this.board
-    var newBikes = this.bikes
+    var newBoard = clone(this.board)
+    var newBikes = clone(this.bikes)
+    var newInputs = []
     for (var i = 0; i < this.bikes.length; i++) {
-      newBikes[i].dir = this.inputs[i]
-      if (this.bikes[i].dir === C.UP) {
-        newBikes[i].i--
-      } else if (this.bikes[i].dir === C.DOWN) {
-        newBikes[i].i++
-      } else if (this.bikes[i].dir === C.RIGHT) {
-        newBikes[i].j--
-      } else if (this.bikes[i].dir === C.LEFT) {
-        newBikes[i].j++
+      if (!(this.inputs[i] === null)) {
+        newBikes[i].dir = this.inputs[i]
       }
-      newBoard[newBikes[i].i][newBikes[i].j] = 1
+
+      if (newBikes[i].dir === C.UP) {
+        newBikes[i].i--
+      } else if (newBikes[i].dir === C.DOWN) {
+        newBikes[i].i++
+      } else if (newBikes[i].dir === C.RIGHT) {
+        newBikes[i].j++
+      } else if (newBikes[i].dir === C.LEFT) {
+        newBikes[i].j--
+      }
+      newBoard[newBikes[i].i][newBikes[i].j] = i + 1
+      newInputs.push(null)
     }
-    return new Turn(newBoard, newBikes, [null])
+    return new Turn(newBoard, newBikes, newInputs)
   }
 }
 exports.Turn = Turn
