1fdfdf49ca975051abf887f7c99146b2f801a541
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2016-06-29 12:39:52 +0200

    updates

---
+++ src/client.js
@@ -0,0 +1,24 @@
+/* const io = require('socket.io-client')
+const socket = io('http://localhost:3000')
+var string = 'nothing'
+
+socket.on('connect', () => {
+  socket.on('ok', () => console.log('all good'))
+})
+
+const KEY = {
+  W: 87,
+  A: 65,
+  S: 83,
+  D: 68
+} */
+
+/* document.addEventListener('keydown', function (e) {
+  switch (e.keyCode) {
+    case KEY.S: string = socket.emit('string', 'Down!')
+    case KEY.S: string = socket.emit('string', 'Up!')
+    case KEY.S: string = socket.emit('string', 'Left!')
+    case KEY.S: string = socket.emit('string', 'Right!')
+  }
+})
+*/
