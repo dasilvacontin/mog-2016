0da0fdeae9e4b1e982c5265caaa02509ad924dd9
Alice <alicenara165@gmail.com>
2016-06-29 11:29:01 +0200

    finished

--- src/Turn.js
+++ src/Turn.js
@@ -1,7 +1,10 @@
+const C = require('../src/constants.js')
 class Turn {
   constructor (board, bikes, inputs) {
     this.board = board
+    this.newboard = board.map(lst => lst.slice())
     this.bikes = bikes
+    this.newbikes = bikes.map(obj => Object.assign({}, obj))
     this.inputs = inputs
     this.blength = board[0].length - 1
   }
@@ -9,81 +12,59 @@ class Turn {
     this.inputs[num] = dir
   }
   isNotOppositeDirection (dir1, dir2) {
-    const sumdirs = dir1 + dir2
-    if (sumdirs === 1 || sumdirs === 5) {
-      return false
-    }
-    return true
+    return (dir1 + dir2 !== C.UP + C.DOWN) && (dir1 + dir2 !== C.RIGHT + C.LEFT)
   }
   killBike (bikeId) {
-    const bike = this.bikes[bikeId]
+    const bike = this.newbikes[bikeId]
     bike.alive = false
-    this.board = this.board.map(lst => lst.map(item => {
-      if (item === bikeId + 1) {
-        return 0
-      } else {
-        return item
-      } }))
+    this.newboard = this.newboard.map(lst => lst.map(item => item === bikeId + 1 ? -1 : item))
   }
   evolve () {
     for (var iter = 0; iter < this.inputs.length; ++iter) {
       const newdir = this.inputs[iter]
-      var olddir = this.bikes[iter].dir
-      console.log(this.isNotOppositeDirection(olddir, newdir))
+      var olddir = this.newbikes[iter].dir
       if (newdir !== null && this.isNotOppositeDirection(olddir, newdir)) {
-        olddir = newdir
+        this.newbikes[iter].dir = newdir
       }
     }
-    for (var newiter = 0; newiter < this.bikes.length; ++newiter) {
-      const bike = this.bikes[newiter]
-      switch (bike.dir) {
-        case 0:
-          bike.i -= 1
-          break
-        case 1:
-          bike.i += 1
-          break
-        case 2:
-          bike.j += 1
-          break
-        case 3:
-          bike.j -= 1
-          break
-        default:
-          throw new Error('Not a valid direction of movement')
-      }
-      /* Out of limits */
-      if (bike.i < 0 || bike.i > this.blength || bike.j < 0 || bike.j > this.blength) {
-        this.killBike(newiter)
-      } else {
-        /* Check if it crashes with other bikes */
-        var actualPos = this.board[bike.i][bike.j]
-        if (actualPos === 0) {
-          actualPos = newiter + 1
-        } else if (actualPos === -1) {
+    for (var newiter = 0; newiter < this.newbikes.length; ++newiter) {
+      var bike = this.newbikes[newiter]
+      if (bike.alive) {
+        switch (bike.dir) {
+          case C.UP: bike.i -= 1; break
+          case C.DOWN: bike.i += 1; break
+          case C.RIGHT: bike.j += 1; break
+          case C.LEFT: bike.j -= 1; break
+          default:
+            throw new Error('Not a valid direction of movement')
+        }
+        this.newbikes[newiter] = bike
+        /* Out of limits */
+        if (bike.i < 0 || bike.i > this.blength || bike.j < 0 || bike.j > this.blength) {
           this.killBike(newiter)
         } else {
-          this.killBike(newiter)
-          /* Num of other bike */
-          actualPos -= 1
-          if (this.bikes[actualPos].i === bike.i && this.bikes[actualPos].j === bike.j) {
-            this.killBike(actualPos)
+          /* Check if it crashes with other bikes */
+          var actualPos = this.newboard[bike.i][bike.j]
+          if (actualPos === 0) {
+            this.newboard[bike.i][bike.j] = newiter + 1
+          } else if (actualPos === -1) {
+            this.killBike(newiter)
+          } else {
+            this.killBike(newiter)
+            /* Num of other bike */
+            actualPos -= 1
+            if (this.newbikes[actualPos].i === bike.i && this.newbikes[actualPos].j === bike.j) {
+              this.killBike(actualPos, this.newbikes, this.newboard)
+            }
           }
-          actualPos = -1
         }
       }
     }
     /* Get rid of -1 */
-    this.board = this.board.map(lst => lst.map(item => {
-      if (item === -1) {
-        return 0
-      } else {
-        return item
-      } }))
-    var newboard = this.board.map(lst => lst.slice())
-    var newbikes = this.bikes.map(obj => Object.assign({}, obj))
+    this.newboard = this.newboard.map(lst => lst.map(item => item === -1 ? 0 : item))
+
     var newinput = this.inputs.map(item => null)
-    return new Turn(newboard, newbikes, newinput)
+    return new Turn(this.newboard, this.newbikes, newinput)
   }
 }
 exports.Turn = Turn
