07294c75cca31509748bce3ed270a9a11d120eeb
David da Silva <dasilvacontin@gmail.com>
2016-07-01 12:40:57 +0200

    Fix test to contemplate delay in socket.emit

--- test/game.spec.js
+++ test/game.spec.js
@@ -321,12 +321,15 @@ test('Game :: change dir for past turn', (t) => {
   // socket2 is requesting to change input to C.RIGHT
   // for turn #2, which is index 1 in turns array
   game.onChangeDir(socket2, C.RIGHT, 1)
-  t.equal(stateReceived, 2, 'players should have been notified of resimulation')
   t.equal(game.turn.board[1][2], 2, 'player two should have moved right on turn 2')
   t.deepEqual(game.turns[1].inputs, [null, C.RIGHT])
   t.deepEqual(game.turns[2].inputs, [C.LEFT, null])
   t.equal(game.turn.bikes[1].i, 1, 'bike pos in turn 2 should be (2,1)')
   t.equal(game.turn.bikes[1].j, 2, 'bike pos in turn 2 should be (2,1)')
   t.equal(game.turn.bikes[1].alive, true, 'bike should be alive in resimulation')
-  t.end()
+
+  setTimeout(() => {
+    t.equal(stateReceived, 2, 'players should have been notified of resimulation')
+    t.end()
+  }, 10)
 })
