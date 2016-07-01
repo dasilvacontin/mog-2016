8780fe88cfe009dc76f2b090fa4d5f3bf46fa9c5
David da Silva <dasilvacontin@gmail.com>
2016-07-01 14:10:02 +0200

    Add chat example code

--- src/client.js
+++ src/client.js
@@ -52,9 +52,41 @@ const DIR_FOR_KEY = {
   [KEY.D]: C.RIGHT
 }
 
+const ENTER = 13
+const chatInput = document.getElementById('chatInput')
 document.addEventListener('keydown', function (e) {
+  if (e.keyCode === ENTER) return sendMessage()
+
   const dir = DIR_FOR_KEY[e.keyCode]
   if (dir == null) return
   const turnIndex = game.turns.length - 1
   socket.emit('changeDir', dir, turnIndex)
 })
+
+/*
+ * const chatSocket = io()
+ * chatSocket.on('chatMessage', (message) => {
+ *
+ * })
+ *
+ * socket.id
+ * #/iuoasdufhna8s3286
+ */
+
+function sendMessage () {
+  let content = chatInput.value.trim()
+  chatInput.value = ''
+  if (content.length === 0) return
+
+  console.log('input', chatInput.value)
+  socket.emit('chatMessage', content)
+
+  const messageDOM = document.createElement('p')
+  messageDOM.innerHTML = `Anon: ${content}`
+  chatContainer.appendChild(messageDOM)
+}
+global.sendMessage = sendMessage
+
+// chat
+const chatContainer = document.getElementById('chatContainer')
+console.log(chatContainer)
